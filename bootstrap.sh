9245041d32a211f0bae8c3add96cf04deeab2114
Marco Blanchette <marcoblanchette@icloud.com>
2015-05-04 23:33:48 -0700

    Updating bootstrap to new BaseSpace spec

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,28 +1,19 @@
-# Adding the basepace repo to the apt-get list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+sudo sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian wheezy/snapshots/3.12.0 main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
 echo deb http://basespace-apt.s3.amazonaws.com spacedock main | sudo tee -a /etc/apt/sources.list
 
 # updating/upgrading the OS
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get -qq -y update
-sudo apt-get -qq -y upgrade
+export DEBIAN_FRONTEND=noninteractive && sudo apt-get -q -y update
+export DEBIAN_FRONTEND=noninteractive && sudo apt-get -q -y upgrade
+
+
+sudo apt-get install -q -y lxc-docker-1.5.0 mono-complete spacedock
+
+echo  'DOCKER_OPTS="-d -H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock"' | sudo tee -a /etc/default/docker
+
+sudo service docker restart
 
 ## Installing emacs and perl
-sudo apt-get install -qq -y emacs24-nox
-
-# Installing the packages (and my favorite text editor)
-sudo apt-get install -qq -y --force-yes mono-complete docker.io
-
-# Linking the apt-get installed mono to /usr/local/bin
-sudo ln -s /usr/bin/mono /usr/local/bin/mono
-
-## Installing SpaceDock
-sudo apt-get install -qq -y --force-yes spacedock
-
-# Configuring docker
-echo  'DOCKER_OPTS="-d -H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock"' | sudo tee -a /etc/default/docker.io 
-
-# Updating security keys
-sudo mozroots --machine --import --sync
-
-# Restarting docker and spacedock services
-sudo service docker.io restart
+##sudo apt-get install -qq -y emacs24-nox
